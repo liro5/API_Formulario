1c6edb3863323849283ea97721518e8fbdfcd0da
Tatiana <gitalosu@gmail.com>
2022-09-01 20:38:24 -0500

    Tabla en js y html

--- main.js
+++ main.js
@@ -6,16 +6,17 @@ request.responseType = "json";
 request.send();
 
 
-function listener(){
+function listener() {
 
-let datos = request.response;
+    let datos = request.response;
 
-  for (let i = 0; i < datos.members.length; i++) {
-    const Heroe = datos.members[i];
-   
-    
-  }
-        
- 
-    
+    for (let i = 0; i < datos.members.length; i++) {
+        const Heroe = datos.members[i];
+        let tabla = `<tr>
+        <td scope='row'>`+ Heroe.name + `</td>
+        <td scope='row'>`+ Heroe.age + `</td>
+        <td scope='row'>`+ Heroe.secretIdentity + `</td>
+        <tr>`
+        document.getElementById("listaHeroes").innerHTML += tabla;
+    }
 }
