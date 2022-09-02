e98603ebf8488dac63dff10127b344b5d5681988
liro5 <lidisrondon@yahoo.es>
2022-09-01 21:12:33 -0500

    Cambio en la tabla, Titulos

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ request.send();
 function listener() {
 
     let datos = request.response;
+    document.getElementById("titulo").innerHTML = datos.squadName;
+    document.getElementById("subtitulo").innerHTML = datos.homeTown;
+    // document.getElementById("subtitulo").innerHTML = datos.homeTown;
+    
 
     for (let i = 0; i < datos.members.length; i++) {
         const Heroe = datos.members[i];
+        
         let tabla = `<tr>
         <td scope='row'>`+ Heroe.name + `</td>
         <td scope='row'>`+ Heroe.age + `</td>
         <td scope='row'>`+ Heroe.secretIdentity + `</td>
+        <td scope='row'>`+ Heroe.powers + `</td>
         <tr>`
         document.getElementById("listaHeroes").innerHTML += tabla;
     }
