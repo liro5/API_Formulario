ac7504804c706dd8b43ed0e435887b5f023e17ba
liro5 <lidisrondon@yahoo.es>
2022-09-01 20:03:11 -0500

    API, funcion lectura de datos

--- main.js
+++ main.js
@@ -2,9 +2,20 @@
 let request = new XMLHttpRequest();
 request.addEventListener("load", listener);
 request.open("GET", "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
+request.responseType = "json";
 request.send();
+
 
 function listener(){
 
-    console.log(this.responseText);
+let datos = request.response;
+
+  for (let i = 0; i < datos.members.length; i++) {
+    const Heroe = datos.members[i];
+   
+    
+  }
+        
+ 
+    
 }
