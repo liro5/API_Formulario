1ae1ba1b906d50a211c7b9cb414512607b264e7c
liro5 <lidisrondon@yahoo.es>
2022-09-01 19:08:23 -0500

    Prueba de API

---
+++ main.js
@@ -0,0 +1,11 @@
+let request = new XMLHttpRequest();
+
+request.addEventListener("load", listener);
+
+request.open("GET", "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
+request.send();
+
+function listener(){
+
+    console.log(this.responseText);
+}
