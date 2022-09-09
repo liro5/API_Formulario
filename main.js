4952b6252f2076e63a0606d1ce2bca088be958ef
liro5 <lidisrondon@yahoo.es>
2022-09-08 20:15:00 -0500

    .

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function darPersonas() {
 }
 
 function listener() {
-    document.getElementById("listaPersonas").innerHTML = "";
+    document.getElementById("listaPersonas").value = "";
     let Personas = request.response;
     for (let i = 0; i < Personas.length; i++) {
         const Persona = Personas[i];
@@ -24,16 +24,13 @@ function listener() {
         <td scope='row'>`+ Persona.direccion + `</td>
         <td scope='row'>`+ Persona.telefono + `</td>
         <td scope='row'>`+ Persona.nacimiento + `</td>
-        <td scope='row'>`+ "<span onclick='eliminar("+ Persona.id + ")' class='btn material-symbols-outlined'>delete</span>" + `</td>
+        <td scope='row'>`+ "<span onclick='eliminar(" + Persona.id + ")' class='colorEliminar btn material-symbols-outlined' >delete</span>" + `</td>
         <tr>`
         document.getElementById("listaPersonas").innerHTML += tabla;
     }
 }
 
 function Agregar() {
-    if (document.getElementById("nombre").required) {
-
-    }
 
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
@@ -50,26 +47,40 @@ function Agregar() {
     if (persona.nombre.length > 2 &&
         persona.apellido.length > 2 &&
         persona.direccion.length > 2 &&
-        persona.telefono.length > 2 
-      ) {
-        // boton de eliminar
+        persona.telefono.length > 2
+    ) {
+
+        let request2 = new XMLHttpRequest();
+        request2.addEventListener("load", darPersonas);
+        request2.open("POST", "http://104.43.223.38/Personas");
+        request2.setRequestHeader("Content-Type", "application/json");
+        request2.send(JSON.stringify(persona));
+
+        limpiarFormulario();
 
     } else {
 
+        alert("Por favor complete todos los campos");
+
     }
-    let request2 = new XMLHttpRequest();
-    request2.addEventListener("load", darPersonas);
-    request2.open("POST", "http://104.43.223.38/Personas");
-    request2.setRequestHeader("Content-Type", "application/json");
-    request2.send(JSON.stringify(persona));
+
 
 }
 function eliminar(id) {
-    
+
     let request3 = new XMLHttpRequest();
     request3.addEventListener("load", darPersonas);
     request3.open("DELETE", "http://104.43.223.38/Personas?idpPrsona=" + id);
     request3.setRequestHeader("Content-Type", "application/json");
     request3.send();
+
+}
+function limpiarFormulario() {
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("direccion").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("nacimiento").value = "";
 
 }
